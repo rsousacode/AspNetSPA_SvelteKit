4876966444810fae016abeea91da0c1f0d2a0d6c
Wvader <34067397+wvader@users.noreply.github.com>
2023-02-12 15:12:31 +0000

    improve

--- AspNetSPA_SvelteKit/ClientApp/src/routes/[...catchall]/+page.server.js
+++ AspNetSPA_SvelteKit/ClientApp/src/routes/[...catchall]/+page.server.js
@@ -1,6 +1,7 @@
+import {error} from "@sveltejs/kit";
+
+
 /** @type {import('./$types').PageLoad} */
-
-
 export async function load({fetch, params}) {
   // Check if slug params exist
   return await fetch("/api/WeatherForecast")
--- AspNetSPA_SvelteKit/ClientApp/src/routes/working/+page.server.js
+++ AspNetSPA_SvelteKit/ClientApp/src/routes/working/+page.server.js
@@ -1,4 +1,8 @@
+import {error} from "@sveltejs/kit";
+
+
 /** @type {import('./$types').PageLoad} */
+
 export async function load({fetch, params}) {
   // Check if slug params exist
   return await fetch("http://localhost:5125/api/WeatherForecast")
